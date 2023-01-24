c2bf418583e5f89cbd981eb2c5f900c77385c50f
hyeona02 <gusdk1418@naver.com>
2023-01-25 00:37:22 +0900

    naverLogin

---
+++ app/src/main/java/com/example/naverlogin/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.naverlogin
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.naverlogin.databinding.ActivityMainBinding
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = this.javaClass.simpleName
+
+    private lateinit var binding: ActivityMainBinding
+    private var email: String = ""
+    private var gender: String = ""
+    private var name: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.run {
+            naverLoginButton.setOnClickListener {
+                val oAuthLoginCallback = object : OAuthLoginCallback {
+                    override fun onSuccess() {
+                        // 네이버 로그인 API 호출 성공 시 유저 정보를 가져온다
+                        NidOAuthLogin().callProfileApi(object :
+                            NidProfileCallback<NidProfileResponse> {
+                            override fun onSuccess(result: NidProfileResponse) {
+                                name = result.profile?.name.toString()
+                                email = result.profile?.email.toString()
+                                gender = result.profile?.gender.toString()
+                                Log.e(TAG, "네이버 로그인한 유저 정보 - 이름 : $name")
+                                Log.e(TAG, "네이버 로그인한 유저 정보 - 이메일 : $email")
+                                Log.e(TAG, "네이버 로그인한 유저 정보 - 성별 : $gender")
+                            }
+
+                            override fun onError(errorCode: Int, message: String) {
+                                //
+                            }
+
+                            override fun onFailure(httpStatus: Int, message: String) {
+                                //
+                            }
+                        })
+                    }
+
+                    override fun onError(errorCode: Int, message: String) {
+                        val naverAccessToken = NaverIdLoginSDK.getAccessToken()
+                        Log.e(TAG, "naverAccessToken : $naverAccessToken")
+                    }
+
+                    override fun onFailure(httpStatus: Int, message: String) {
+                        //
+                    }
+                }
+
+                NaverIdLoginSDK.initialize(
+                    this@MainActivity,
+                    getString(R.string.naver_client_id),
+                    getString(R.string.naver_client_secret),
+                    "앱 이름"
+                )
+                NaverIdLoginSDK.authenticate(this@MainActivity, oAuthLoginCallback)
+            }
+        }
+    }
+}
+
+
+
