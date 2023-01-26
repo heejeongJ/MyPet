c49e6f9fde3809f7b99b8bdcfe07df0a5df4ed7a
sunjung-Dev <sun17789@naver.com>
2023-01-26 21:35:38 +0900

    To go login page, use Intent

--- app/src/main/java/com/example/mypet/MainActivity.kt
+++ app/src/main/java/com/example/mypet/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.mypet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var go = findViewById<Button>(R.id.button5)
+        go.setOnClickListener{
+            val nextIntent = Intent(this, SignUpNLogIn::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
