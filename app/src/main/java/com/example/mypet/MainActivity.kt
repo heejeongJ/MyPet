3b2aab2fbb53f95370e4fff9a29ee2da0d6b392e
Yunju12 <yunju.tech@gmail.com>
2023-01-26 16:23:22 +0900

    mapViewEvent

--- app/src/main/java/com/example/mypet/MainActivity.kt
+++ app/src/main/java/com/example/mypet/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mypet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import net.daum.mf.map.api.MapView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapView = MapView(this)
+
+        val mapViewContainer = map_view as ViewGroup
+        mapViewContainer.addView(mapView)
     }
 }
