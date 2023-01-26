733f61e299105f0b594b218953d99e3c91d419a7
JooHeeJeong <heeej920@naver.com>
2023-01-26 20:11:48 +0900

    sign up and log in page

---
+++ app/src/main/java/com/example/mypet/SignUp_sub1.kt
@@ -0,0 +1,11 @@
+package com.example.mypet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp_sub1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_sub1)
+    }
+}
