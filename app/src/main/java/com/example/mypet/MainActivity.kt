8e6be92fc74fe1e7ce61635315e9ef46bf777266
sunjung-Dev <sun17789@naver.com>
2023-01-26 23:32:06 +0900

    로그인 기능 쿼리 완료

--- app/src/main/java/com/example/mypet/MainActivity.kt
+++ app/src/main/java/com/example/mypet/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mypet
 
+import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,7 +15,6 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         var go = findViewById<Button>(R.id.button5)
         go.setOnClickListener{
             val nextIntent = Intent(this, SignUpNLogIn::class.java)
