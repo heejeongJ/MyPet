32d868289d8b16affbeda5b3b0d9de98bb933207
Yunju12 <yunju.tech@gmail.com>
2023-01-27 01:05:48 +0900

    naverMapMaker

--- app/src/main/java/com/example/mypet/LocationTrackingActivity.kt
+++ app/src/main/java/com/example/mypet/LocationTrackingActivity.kt
@@ -1,13 +1,17 @@
 package com.example.mypet
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 
 open class LocationTrackingActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var locationSource: FusedLocationSource
+    private lateinit var locationSource: FusedLocationSource // 위치 반환
     private lateinit var naverMap: NaverMap
 
     companion object {
@@ -26,8 +30,7 @@ open class LocationTrackingActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this) // 인터페이스 역할을 하는 naverMap 객체를 얻어옴
 
         // 지도에 현재 위치 추가
-        locationSource =
-            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
     }
 
     // 네이버 지도 뷰를 생성
@@ -42,6 +45,9 @@ open class LocationTrackingActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
         naverMap.locationSource = locationSource
+        naverMap.maxZoom = 18.0 // zoom max/min 제한
+        naverMap.minZoom = 10.0
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
         requestPermissions(PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE) // 사용자에게 권한을 요청함
     }
 
@@ -54,10 +60,22 @@ open class LocationTrackingActivity : AppCompatActivity(), OnMapReadyCallback {
                 naverMap.locationTrackingMode = LocationTrackingMode.None
                 return
             }
-            else{ // 권한이 인가되었다면
+            else{ // 권한이 허용되었다면
                 naverMap.locationTrackingMode = LocationTrackingMode.Follow
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    // 네이버 지도에 마커를 표시
+    fun createMarkerOnMap(lat : Double, log : Double, label : String) {
+        val marker = Marker()
+        marker.position = LatLng(lat, log) // 마커의 좌표 설정
+        // 마커의 색상 설정
+        marker.icon = MarkerIcons.BLACK
+        marker.iconTintColor = Color.RED
+        marker.map = naverMap
+        // 마커의 라벨 설정
+        marker.captionText = label
     }
 }
